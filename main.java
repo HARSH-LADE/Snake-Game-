49294f4cd1e5f8864b09c31559f7f4659c6eee49
HARSH-LADE <148345277+HARSH-LADE@users.noreply.github.com>
2023-10-23 16:11:33 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+
+ import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setBounds(10,10,905,700);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        GamePanel panel = new GamePanel();
+        panel.setBackground(Color.black);
+        frame.add(panel);
+
+        frame.setVisible(true);
+
+
+    }
+}
